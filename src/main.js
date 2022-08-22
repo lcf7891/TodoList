770c7a0992435d5f045bcafc8b71a01957b46916
Allen <lcf7891@gmail.com>
2022-08-22 23:54:30 +0800

    add matterList.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 // 載入圖片
 import './assets/images/image.png'
 import './assets/images/logo_lg.png'
+import './assets/images/empty.png'
 // 載入 CSS
 import './assets/scss/all.scss'
 
 // import { url } from './assets/js/AjaxApi'
 import { login } from './assets/js/Login'
 import { SignUp }  from './assets/js/SignUp'
+import { noneList } from './assets/js/matterList'
 
-const userControl = document.getElementById('js-user-control')
-// const globalControl = document.getElementById('js-global-control')
+// const userControl = document.getElementById('js-user-control')
+const globalControl = document.getElementById('js-global-control')
 // console.log(url)
 // console.log(userControl)
 // console.log(globalControl)
 console.log(login)
+console.log(SignUp)
 
-userControl.innerHTML = SignUp
+globalControl.innerHTML = noneList
 
