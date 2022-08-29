7a9a2ded32ef7f0bcc726800ae899ca31993b728
Allen <lcf7891@gmail.com>
2022-08-29 22:32:46 +0800

    Use the axios tool and refactor

--- src/main.js
+++ src/main.js
@@ -10,53 +10,47 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 
 // 載入版型 JS 
 import { startPage, logIn, signUp, initList, noneList, dateList } from './assets/js/Templates'
+
+// 載入 AJAX 工具
 // import { axios } from '../node_modules/axios/dist/axios'
 const axios = require('axios').default;
 
+// API 網址
 const apiUrl = 'https://todoo.5xcamp.us/'
 
+// 使用者資料變數
 let user = {}
 
-function Rendering() {
-  // 將登入頁面載入
-  const globalControl = document.getElementById('js-global-control')
-  globalControl.innerHTML = startPage
+// 宣告初始 DOM 元素控制
+const globalControl = document.getElementById('js-global-control')
 
-  // 登入版型載入
+// 初始渲染
+function Rendering() {
+  // 初始化頁面
+  globalControl.innerHTML = startPage
+  // 加載登入版型
   const startLogin = document.getElementById('js-user-control')
   startLogin.innerHTML = logIn
-
-  // 監聽登入按鈕
+  // 建立登入按鈕監聽
   const addBtn = document.querySelector('[type="submit"]')
-  addBtn.addEventListener('click', (e) => {
+  addBtn.addEventListener('click', () => {
     user.email = document.querySelector('[type="email"]').value.trim()
     user.password = document.querySelector('[type="password"]').value.trim()
-    // bootstrap 驗證
+    // 登入輸入框驗證
     formValidation()
-    // 串接登入 API
-    userLogin(user, globalControl)
+    // 登入 AJAX
+    userLogin()
   })
-
-  // 切換註冊畫面
-  const changeSignUp = document.querySelector('form a')
-  changeSignUp.addEventListener('click', () => {
-    startLogin.innerHTML = signUp
-    registerInfo(startLogin)
-  })
+  // 建立註冊帳號按鈕監聽
+  changeUserInfo()
+  // const changeBtn = document.querySelector('form a')
+  // changeBtn.addEventListener('click', () => {
+  //   // 註冊 AJAX
+  //   userSignUp(startLogin)
+  // })
 }
 
-function checkPermissions() {
-  fetch(`${apiUrl}check`, {
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'GET'
-  })
-    .then(response => response.json())
-    .then(data => console.log(data))
-  console.log('g')
-}
-
+// bootstrap form 驗證
 function formValidation() {
   const forms = document.querySelectorAll('.needs-validation')
   // Loop over them and prevent submission
@@ -72,110 +66,196 @@ function formValidation() {
     })
 }
 
-function userLogin(user, globalControl) {
-  const errMessage = document.querySelector('.errMessage')
-  errMessage.style.display = 'none'
-  fetch(`${apiUrl}users/sign_in`, {
-    method: 'POST',
-    body: JSON.stringify({user}),
-    headers: {
-      'content-type': 'application/json'
-    },
-  })
-    .then(Response => Response.json())
-    .then(data => {
-      console.log(data)
-      if(data.message === '登入成功') {
-        globalControl.innerHTML =initList
-        userList(data.nickname)
-        // checkPermissions()
-        // globalControl.innerHTML = initList
-      } else if(data.message === '登入失敗') {
-        errMessage.style.display = 'block'
-      }
-    })
-    .catch(error => console.log('錯誤資訊：', error))
-}
-
-function registerInfo(startLogin) {
-  const registerBtn = document.querySelector('[type="submit"]')
-  registerBtn.addEventListener('click', () => {
-    user.email = document.querySelector('[type="email"]').value.trim()
-    user.nickname = document.getElementById('nickName').value.trim()
-    const password = document.querySelector('[type="password"]').value.trim()
-    const passwords = document.getElementById('loginPasswords').value.trim()
-    const errMessage = document.querySelector('.errMessage')
-    errMessage.style.display = 'none'
-    if(password === passwords) {
-      // bootstrap 驗證
-      formValidation()
-      // 串接註冊 API
-      userSignUp(user, startLogin)
+// 登入與註冊頁面切換
+function changeUserInfo(state) {
+  const changeBtn = document.querySelector('form a')
+  changeBtn.addEventListener('click', () => {
+    if(state === 'in') {
+      // 登入 AJAX
+      userLongin()
     } else {
-      document.getElementById('loginPasswords').classList.add('is-invalid')
-      errMessage.innerHTML = `<p>輸入第 2 次的密碼與第 1 次的不相同</p>`
-        errMessage.style.display = 'block'
+      // 註冊 AJAX
+      userSignUp()
     }
   })
-
-  // 切換註冊畫面
-  const changeLogIn = document.querySelector('form a')
-  changeLogIn.addEventListener('click', () => {
-    Rendering()
-  })
 }
 
-function userSignUp(user, startLogin) {
-  const errMessage = document.querySelector('.errMessage')
-  errMessage.style.display = 'none'
-  fetch(`${apiUrl}users`, {
-    method: 'POST',
-    body: JSON.stringify({user}),
-    headers: {
-      'content-type': 'application/json'
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      if(data.message === '註冊成功') {
-        errMessage.innerHTML = `
-          <p>${data.message}</p>
-          <p>將在 5 秒後跳轉至登入頁面</p>
-        `
-        errMessage.style.display = 'block'
-
-        // 切換登入畫面
-        setTimeout(() => {
-          startLogin.innerHTML = logIn
-        }, 5000)  
-      } else if(data.message === '註冊發生錯誤') {
-        errMessage.innerHTML = `<p>${data.error[1]}。${data.error[3]}</p>`
-        errMessage.style.display = 'block'
-      }
+// 登入 AJAX
+function userLogin() {
+  axios.post(`${apiUrl}users/sign_in`, { user })
+    .then(response => {
+      console.log(response)
     })
-    .catch(error => console.log('錯誤資訊：', error))
 }
 
-function userList(nickname) {
-  // 將暱稱改成使用者暱稱
-  const nickName = document.querySelector('span')
-  nickName.innerText = nickname
-  // 宣告列表 DOM 元素的控制
-  const listControl = document.getElementById('js-list-control')
-  console.log(listControl)
+// 註冊 AJAX
+function userSignUp(startLogin) {
+  startLogin.innerHTML = signUp
+  console.log('signUp')
 }
-
-axios.post(`${apiUrl}users/sign_in`, {
-  user: {
-    email: 'qwe789@456.asd',
-    password: 'qsc753'
-  }
-})
-  .then(response => {
-    console.log(response)
-    const token = response.headers.authorization
-    console.log(response.headers.authorization)
-  })
-  .catch(error => console.log('錯誤訊息：', error))
 
 Rendering()
+
+
+// function Rendering() {
+//   // 將登入頁面載入
+//   const globalControl = document.getElementById('js-global-control')
+//   globalControl.innerHTML = startPage
+
+//   // 登入版型載入
+//   const startLogin = document.getElementById('js-user-control')
+//   startLogin.innerHTML = logIn
+
+//   // 監聽登入按鈕
+//   const addBtn = document.querySelector('[type="submit"]')
+//   addBtn.addEventListener('click', (e) => {
+//     user.email = document.querySelector('[type="email"]').value.trim()
+//     user.password = document.querySelector('[type="password"]').value.trim()
+//     // bootstrap 驗證
+//     formValidation()
+//     // 串接登入 API
+//     userLogin(user, globalControl)
+//   })
+
+//   // 切換註冊畫面
+//   const changeSignUp = document.querySelector('form a')
+//   changeSignUp.addEventListener('click', () => {
+//     startLogin.innerHTML = signUp
+//     registerInfo(startLogin)
+//   })
+// }
+
+// function checkPermissions() {
+//   fetch(`${apiUrl}check`, {
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//     method: 'GET'
+//   })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//   console.log('g')
+// }
+
+// function formValidation() {
+//   const forms = document.querySelectorAll('.needs-validation')
+//   // Loop over them and prevent submission
+//   Array.prototype.slice.call(forms)
+//     .forEach(form => {
+//       form.addEventListener('submit', function (event) {
+//         if (!form.checkValidity()) {
+//           event.preventDefault()
+//           event.stopPropagation()
+//         }
+//         form.classList.add('was-validated')
+//       }, false)
+//     })
+// }
+
+// function userLogin(user, globalControl) {
+//   const errMessage = document.querySelector('.errMessage')
+//   errMessage.style.display = 'none'
+//   fetch(`${apiUrl}users/sign_in`, {
+//     method: 'POST',
+//     body: JSON.stringify({user}),
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//   })
+//     .then(Response => Response.json())
+//     .then(data => {
+//       console.log(data)
+//       if(data.message === '登入成功') {
+//         globalControl.innerHTML =initList
+//         userList(data.nickname)
+//         // checkPermissions()
+//         // globalControl.innerHTML = initList
+//       } else if(data.message === '登入失敗') {
+//         errMessage.style.display = 'block'
+//       }
+//     })
+//     .catch(error => console.log('錯誤資訊：', error))
+// }
+
+// function registerInfo(startLogin) {
+//   const registerBtn = document.querySelector('[type="submit"]')
+//   registerBtn.addEventListener('click', () => {
+//     user.email = document.querySelector('[type="email"]').value.trim()
+//     user.nickname = document.getElementById('nickName').value.trim()
+//     const password = document.querySelector('[type="password"]').value.trim()
+//     const passwords = document.getElementById('loginPasswords').value.trim()
+//     const errMessage = document.querySelector('.errMessage')
+//     errMessage.style.display = 'none'
+//     if(password === passwords) {
+//       // bootstrap 驗證
+//       formValidation()
+//       // 串接註冊 API
+//       userSignUp(user, startLogin)
+//     } else {
+//       document.getElementById('loginPasswords').classList.add('is-invalid')
+//       errMessage.innerHTML = `<p>輸入第 2 次的密碼與第 1 次的不相同</p>`
+//         errMessage.style.display = 'block'
+//     }
+//   })
+
+//   // 切換註冊畫面
+//   const changeLogIn = document.querySelector('form a')
+//   changeLogIn.addEventListener('click', () => {
+//     Rendering()
+//   })
+// }
+
+// function userSignUp(user, startLogin) {
+//   const errMessage = document.querySelector('.errMessage')
+//   errMessage.style.display = 'none'
+//   fetch(`${apiUrl}users`, {
+//     method: 'POST',
+//     body: JSON.stringify({user}),
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       if(data.message === '註冊成功') {
+//         errMessage.innerHTML = `
+//           <p>${data.message}</p>
+//           <p>將在 5 秒後跳轉至登入頁面</p>
+//         `
+//         errMessage.style.display = 'block'
+
+//         // 切換登入畫面
+//         setTimeout(() => {
+//           startLogin.innerHTML = logIn
+//         }, 5000)  
+//       } else if(data.message === '註冊發生錯誤') {
+//         errMessage.innerHTML = `<p>${data.error[1]}。${data.error[3]}</p>`
+//         errMessage.style.display = 'block'
+//       }
+//     })
+//     .catch(error => console.log('錯誤資訊：', error))
+// }
+
+// function userList(nickname) {
+//   // 將暱稱改成使用者暱稱
+//   const nickName = document.querySelector('span')
+//   nickName.innerText = nickname
+//   // 宣告列表 DOM 元素的控制
+//   const listControl = document.getElementById('js-list-control')
+//   console.log(listControl)
+// }
+
+// axios.post(`${apiUrl}users/sign_in`, {
+//   user: {
+//     email: 'qwe789@456.asd',
+//     password: 'qsc753'
+//   }
+// })
+//   .then(response => {
+//     console.log(response)
+//     const token = response.headers.authorization
+//     console.log(response.headers.authorization)
+//   })
+//   .catch(error => console.log('錯誤訊息：', error))
+
+// Rendering()
