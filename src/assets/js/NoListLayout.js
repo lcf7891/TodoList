6b3a678e6216879b0c1c4d1c421277ff04ec190b
Allen <lcf7891@gmail.com>
2022-09-05 13:38:55 +0800

    Complete all layouts, delete unused data

---
+++ src/assets/js/NoListLayout.js
@@ -0,0 +1,8 @@
+const noneList = `
+  <div class="text-center my-15">
+    <p class="fs-5">目前尚無待辦事項</p>
+    <img class="img-fluid" src="./assets/images/empty.png" alt="empty">
+  </div>
+`
+
+export { noneList }
