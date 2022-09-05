5f05a6548ccb13de034bba85847e2ec47a9f0fbd
Allen <lcf7891@gmail.com>
2022-09-05 12:00:06 +0800

    Complete todo edit delete button

--- src/assets/js/InitListLayout.js
+++ src/assets/js/InitListLayout.js
@@ -17,7 +17,7 @@ const initList = `
       <section class="col-md-10 mb-1">
         <div class="input-group shadow-sm mb-3">
           <input type="text" class="form-control form-control-lg border-0 py-3" placeholder="新增代辦事項">
-          <span class="input-group-text addTodoBtn" id="addTodoBtn"><i class="bi bi-plus"></i></span>
+          <span class="input-group-text addTodoBtn" id="addTodoBtn"><i class="bi bi-plus-lg"></i></span>
         </div>
       </section>
       <section class="col-md-10" id="js-list-control"></section>
